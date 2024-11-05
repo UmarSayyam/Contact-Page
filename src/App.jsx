23f6480ed29b268b5606817e51f94fca2a665cb0
Umar Sayyam <umarsayyam123456@gmail.com>
2024-11-05 02:49:06 -0800

    header done for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import ContactHeader from './components/ContactHeader/ContactHeader'
+import Navigation from './components/Navigation/Navigation'
 
 function App() {
-  return <div></div>
+  return <div>
+    <Navigation/>
+    <ContactHeader />
+  </div>
 }
 
 export default App
